3c13a9d0c396fa4b9fbb6d0bedeb91f3462eaf17
EijiSambei <sambei13579@gmail.com>
2022-12-16 00:06:36 +0900

    add

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import web3 from "./web3";
 import contract from './contract';
 import sha256 from 'crypto-js/sha256';
 
-function App() {
+document.getElementById("dummy").remove();
 
+
+function App() {
     return (
         <div>
             <BuyTicket />
@@ -16,60 +18,34 @@ function App() {
 }
 
 
-
 const BuyTicket = () => {
-    const [studentNumber, setStudentNumber] = React.useState("");
-    const [password, setPassword] = React.useState("");
-
-    const [statusNum, setStatusNum] = React.useState("");
-    const [statusPass, setStatusPass] = React.useState("");
-
+    const [ studentNumber, setStudentNumber ] = React.useState("");
+    const [ password, setPassword ] = React.useState("");
+    const [ statusNum, setStatusNum ] = React.useState("");
+    const [ statusPass, setStatusPass ] = React.useState("");
     const [ value, setValue ] = React.useState("");
-
-    const onChangeRadio = (e) => {
-      setValue(e.target.value);
-    };
 
     const onClick = async () => {
       var reg = new RegExp(/^[a-zA-Z0-9]{8}$/); // 半角英数字8文字のみ
       var response = reg.test(studentNumber);
-
       var reg2 = new RegExp(/^[a-zA-Z0-9]*$/); //　半角英数字のみ
       var response2 = reg2.test(password);
 
-
-      // if (!response) {
-      //   setStatusNum("エラー：学籍番号を正しく入力してください。");
-      //   setStatusNum(false);
-      // } else {
-      //   setStatusNum(true);
-      // }
-
-      // if (!response2) {
-      //   setStatusPass("エラー：パスワードを正しく入力してください。");
-      //   setStatusPass(false);
-      // } else {
-      //   setStatusPass(true);
-      // }
-
-      
+      if (!response) setStatusNum("エラー：学籍番号を正しく入力してください。");
+      if (!response2) setStatusPass("エラー：パスワードを正しく入力してください。");
 
       console.log(value, studentNumber, password);
       const userhash =sha256(studentNumber+password).toString();
       console.log(userhash);
 
-      
-      const accounts = await web3.eth.getAccounts();
-      await contract.methods.registerFirstBuyer(userhash).send({    //  userhash
-        from: accounts[0],
-        value: 3000000000000000             // ぴったりに要修正
-      });
-
+      if (response && response2) {
+        const accounts = await web3.eth.getAccounts();
+        await contract.methods.registerFirstBuyer(userhash).send({    //  userhash
+          from: accounts[0],
+          value: 3000000000000000             // ぴったりに要修正
+        });
       }
-
-
-        
-    
+    }
 
     return (
         <div>
@@ -77,7 +53,7 @@ const BuyTicket = () => {
             <p>１．購入する期間を選択してください</p>
             <br/>
 
-            <div onChange={onChangeRadio}>
+            <div onChange={(event) => setValue(event.target.value)}>
                 <label><input type="radio" name="options" value="first" /> 前期: 4月～9月   (価格: 0.003 eth)</label>
                 <br/>
                 <label><input type="radio" name="options" value="second" /> 後期:10月～3月  (価格: 0.003 eth)</label>
@@ -113,10 +89,10 @@ const BuyTicket = () => {
     );
 }
 
+
 const PurchaseStatus = () => {
     const [studentNumber, setStudentNumber] = React.useState("");
     const [password, setPassword] = React.useState("");
-
     const [statusNum, setStatusNum] = React.useState("");
     const [statusPass, setStatusPass] = React.useState("");
 
@@ -124,25 +100,18 @@ const PurchaseStatus = () => {
 
         var reg = new RegExp(/^[a-zA-Z0-9]{8}$/); //上記の正規表現を引数に渡す
         var response = reg.test(studentNumber);
-  
         var reg2 = new RegExp(/^[a-zA-Z0-9]*$/); //上記の正規表現を引数に渡す
         var response2 = reg2.test(password);
   
-        console.log(response, response2);
-  
         if (!response) {
           setStatusNum("エラー：学籍番号を正しく入力してください。");
-        } else {
-          setStatusNum(true);
-        }
+        } 
   
         if (!response2) {
           setStatusPass("エラー：パスワードを正しく入力してください。");
-        } else {
-          setStatusPass(true);
-        }
-
+        } 
     }
+
     return (
         <div>
             <h2>購入状況</h2>
@@ -172,16 +141,6 @@ const PurchaseStatus = () => {
 }
 
 
-// const test = async () => {
-//   const accounts = await web3.eth.getAccounts();
 
-//   const result = await contract.methods.registerFirstBuyer("b43cb10c0c486a6383325e5dbb9b6b88e7152f0ab12c7ba4ddaa1345c724afa1").send({
-//     from: accounts[0],
-//     value: 6000000000000000
-//   });
-
-//   console.log(result);
-// };
-// test();
 
 export default App;
